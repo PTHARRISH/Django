ca1c507acab27ff21f6e471d6d8bb2cbbdec54e8
PTHARRISH <harrishpt@gmail.com>
2024-01-10 21:03:52 +0530

    add model

--- Apps/models.py
+++ Apps/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
-class student(models.Model): # models.Model is django models is a class and Model is a subclass
+class students(models.Model): # models.Model is django models is a class and Model is a subclass
     firstname=models.CharField(max_length=100)
-    
+    start_date=models.DateField()
+    end_date=models.DateField()
+    comments=models.CharField(max_length=500,blank=True,null=True) # blank is True because it is not manditory field
+    status=models.CharField(max_length=100)
+    created=models.DateTimeField(auto_now_add=True)
+    modified=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.firstname
