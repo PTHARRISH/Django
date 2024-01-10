0c65250f45d3eb3c52a0f08bb38074cb6b5fb04f
PTHARRISH <harrishpt@gmail.com>
2024-01-10 21:03:28 +0530

    db class

--- coursera/models.py
+++ coursera/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model): # models.Model is django models is a class and Model is a subclass
+    firstname=models.CharField(max_length=100)
+    
