14f2815d12adbcfb4caa0b5c895a4fb85864b491
PTHARRISH <harrishpt@gmail.com>
2024-01-29 23:48:38 +0530

    loader get_template

--- w3/views.py
+++ w3/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader # display html file 
 
 # Create your views here.
 def members1(request):
