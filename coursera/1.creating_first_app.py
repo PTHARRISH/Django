0f1a32787e1f9bcf693379180d5901666a9ae697
PTHARRISH <harrishpt@gmail.com>
2024-02-05 22:15:11 +0530

    Django notes

---
+++ coursera/1.creating_first_app.py
@@ -0,0 +1,27 @@
+# Creating the first Apps in Django
+# To create your first Django project using the terminal and the VS Code editor. 
+# You will also learn how to set up the development environment, by installing a virtual environment,
+
+# virtual environment: 
+# Some common CLI commands used to create Django projects and launched the development server.
+# If you are a Python developer working on multiple projects, 
+# It's best practice to keep your projects isolated in a virtual environment, 
+# which you may be wondering why you need to do this. 
+# Django projects can often be quite large and involve specific dependencies for things like packages. 
+# For example you could have a package that has a dependency on a specific version.
+# You do not want this conflicting with your other Python or 
+# Django projects so it's best to keep your projects isolated using a virtual development environment. 
+# Virtual environments are isolated spaces you create to manage dependencies and the overall project. 
+# This allows you to keep features such as the interpreter, libraries and scripts 
+# isolated and installed for a specific project. 
+
+
+# Development Server
+# Django also comes with an integrated development server. 
+# This means that the application has a request response relationship with a client. 
+# Now that you are familiar with the concept of the virtual development environment and development server 
+# let's explore the steps involved in creating a Django project. 
+# First, you create a project directory. 
+# Next, you create a virtual environment for your Django application. 
+# Then you create a Django project and finally run the Django development server.
+
--- coursera/views.py
+++ coursera/views.py
@@ -5,3 +5,4 @@ from django.http import HttpResponse
 def home(request):
     content="<html><body><h1>Welcome little lemon</h1></body></html>"
     return HttpResponse(content)
+
