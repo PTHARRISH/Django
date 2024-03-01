e899fdd0c3132689ec4998febb6861342feda08e
PTHARRISH <harrishpt@gmail.com>
2024-03-01 22:57:17 +0530

    django codes and uses

---
+++ Django notes/django_commands.py
@@ -0,0 +1,17 @@
+# 1.Check
+# Check Commands basically check the entire Django Project for Potential
+# problem
+# python manage.py check
+
+# 2.Compilemessages
+# Compiles .po files to .mo files for use with builtin gettext support
+# python manage.py compliemessages
+
+# 3.Create cachetable
+# This command is responsible for creating a cache table in django application
+# python manage.py createcachetable
+
+# 4.dbshell
+# Run the commandline client for specified database or the default database if 
+# none is provided
+# python manage.py dbshell
